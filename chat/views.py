21b23b0cf7756851563bebeb15326a8b429300a0
Ilya <vensilya@gmail.com>
2016-07-26 14:06:00 +0300

    auth validation

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
+    if not request.user.is_authenticated():
+        return redirect('/accounts/login')
+
     context = {
-        'users': User.objects.all() 
+        'users': User.objects.all()
     }
     return render(request, 'chat_homepage.html', context)

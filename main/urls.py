73d59e77fdae619586bf33bebc74c73a42efd5b9
Ilya <vensilya@gmail.com>
2016-07-26 11:30:57 +0300

    chat app init

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""chat URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/

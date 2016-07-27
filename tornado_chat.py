a0a35358bfed4f480a0541ac177ec0bd289e6280
Ilya <vensilya@gmail.com>
2016-07-27 13:02:25 +0300

    switched from brukva to tornado-redis

--- tornado_chat.py
+++ tornado_chat.py
@@ -1,12 +1,14 @@
 import os.path
-import json
 
-import brukva
-import tornado.ioloop
+import tornado.httpserver
 import tornado.web
 import tornado.websocket
+import tornado.ioloop
+import tornado.gen
 
-c = brukva.Client()
+import tornadoredis
+
+c = tornadoredis.Client()
 c.connect()
 
 class Application(tornado.web.Application):
@@ -30,43 +32,36 @@ class ChatHandler(tornado.web.RequestHandler):
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def __init__(self, *args, **kwargs):
         super(WebSocketHandler, self).__init__(*args, **kwargs)
-        self.client = brukva.Client()
-        self.client.connect()
+        self.listen()
 
-    def open(self):
-        self.channel = '1'
-        self.client.subscribe(self.channel)
+    @tornado.gen.engine
+    def listen(self):
+        self.client = tornadoredis.Client()
+        self.client.connect()
+        yield tornado.gen.Task(self.client.subscribe, 'test_channel')
         self.client.listen(self.show_new_message)
 
     def on_message(self, msg):
-        c.publish(self.channel, msg)
+        c.publish('test_channel', msg)
 
-    def show_new_message(self, result):
-        self.write_message(result.body)
+    def show_new_message(self, msg):
+        if msg.kind == 'message':
+            self.write_message(str(msg.body))
+        if msg.kind == 'disconnect':
+            self.write_message('The connection terminated '
+                               'due to a Redis server error.')
+            self.close()
 
     def on_close(self):
-        try:
-            self.client.unsubscribe(self.channel)
-        except AttributeError:
-            pass
-        def check():
-            if self.client.connection.in_progress:
-                tornado.ioloop.IOLoop.instance().add_timeout(
-                    datetime.timedelta(0.00001),
-                    check
-                )
-            else:
-                self.client.disconnect()
-        tornado.ioloop.IOLoop.instance().add_timeout(
-            datetime.timedelta(0.00001),
-            check
-        )
+        if self.client.subscribed:
+            self.client.unsubscribe('test_channel')
+            self.client.disconnect()
 
 
 def main():
-    port = int(os.environ.get("PORT", 8000))
     app = Application()
-    app.listen(port)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
 
 

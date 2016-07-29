83c459237648d448893fdb3abe54d8aa42b9bbb6
Ilya <vensilya@gmail.com>
2016-07-29 11:16:37 +0300

    update chat models

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from chat.models import Message
+from chat.models import Message, Chat
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ['__unicode__']
 
 admin.site.register(Message, MessageAdmin)
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__']
+
+admin.site.register(Chat, ChatAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Message(models.Model):
     text = models.CharField(max_length=3000)
+    sender = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.text
 
     class Meta:
         verbose_name = 'Message'
+        ordering = ['-timestamp']
+
+
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    participants = models.ManyToManyField(User)
+    messages = models.ManyToManyField(Message)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.id
+
+    class Meta:
+        verbose_name = 'Chat'
+        ordering = ['-timestamp']
